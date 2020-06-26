d1c1daee7e3384d52da75cb6eaa98ea9dff3fb52
TesslaRay <cristian.valdivia@alumnos.usm.cl>
2020-06-26 00:38:35 -0400

    add Testing front

--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 // export const GATEWAY_URL = 'http://localhost:5000';
-export const GATEWAY_URL = 'https://dev-api-gateway-dot-ioled-dev-262215.appspot.com';
+// export const GATEWAY_URL = 'https://dev-api-gateway-dot-ioled-dev-262215.appspot.com';
 // export const GATEWAY_URL = 'https://api-gateway-dot-ioled-dev-262215.appspot.com';
+export const GATEWAY_URL = 'https://testing-api-gateway-dot-ioled-dev-262215.appspot.com/';
